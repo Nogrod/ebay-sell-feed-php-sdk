559e836495f4b8fac4a9f5d12e27077d7595f274
Nogrod <strikeomania@gmail.com>
2025-03-13 12:38:34 +0100

    php 8

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 rem openapi request body missing
-java -jar openapi-generator-cli-7.10.0.jar generate -i sell_feed_v1_oas3.yaml -g php-nextgen --git-user-id nogrod --git-repo-id ebay-sell-feed-php-sdk --artifact-version 0.0.1 --enable-post-process-file --additional-properties invokerPackage=eBay\Sell\Feed
+java -jar openapi-generator-cli.jar generate -i sell_feed_v1_oas3.yaml -g php-nextgen --git-user-id nogrod --git-repo-id ebay-sell-feed-php-sdk --artifact-version 0.0.1 --enable-post-process-file --additional-properties invokerPackage=eBay\Sell\Feed
 pause
